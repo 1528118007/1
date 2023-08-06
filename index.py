168a234bbd6b0c146480e3305c29990cdc534050
username-1a <141487950+username-1a@users.noreply.github.com>
2023-08-06 16:09:40 +0800

    Update index.py

--- index.py
+++ index.py
@@ -11,13 +11,13 @@ def calc_steps(z,c,max_iter_num=128):
 
 def display_julia(x_num=100,y_num=100):
   X,Y=np.meshgrid(np.linspace(-2,2,x_num+1),np.linspace(-2,2,y_num+1)
-C=X+Y*1j
-result=np.zeros((y_num+1,x_num+1))
-c=complex(round(random.uniform(-2,2),5))
-for i in range(y_num+1):
-  for j in range(x_num+1):
-    result[i,j]=calc_steps(C[i,j],c)
-plt.imshow(result,interpolation="bilinear",cmap=plt.cm.bone,
+  C=X+Y*1j
+  result=np.zeros((y_num+1,x_num+1))
+  c=complex(round(random.uniform(-2,2),5))
+  for i in range(y_num+1):
+    for j in range(x_num+1):
+      result[i,j]=calc_steps(C[i,j],c)
+  plt.imshow(result,interpolation="bilinear",cmap=plt.cm.bone,
            vmax=abs(result).max(),vmin=abs(result).min(),
            extent=[-2,2,-2,2]
-plt.show()
+  plt.show()
